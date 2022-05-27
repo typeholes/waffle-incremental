bccff0c31fd57e9f981994d44893cb805f79e5aa
typeholes <typeholes@protonmail.com>
2022-05-27 16:27:00 -0400

    fixed url base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: "/waffle-incremental/",
+  plugins: [vue()],
+  build: {
+    assetsDir: "waffle-incremental",
+  },
+});
